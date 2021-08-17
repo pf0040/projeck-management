d3682bfde3979542ad7cedbde5830f01c70f6cf3
Paul Sama <fosehpaul@gmail.com>
2021-08-17 15:34:00 +0000

    sample.sh was created

---
+++ sample2.sh
@@ -0,0 +1,9 @@
+echo "Welcome to Chase Bank of OKC"
+echo "What is your pin?"
+read pin
+echo "Please enter your name to proceed"
+read name
+echo "Thank $name, you entered $pin as your pin"
+echo "Is the $pin you entered correct?"
+read remark
+echo "Thanks for your asnwers $name. You can now access your account"
